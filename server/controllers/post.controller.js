3a7f6a2331ea73b3b787bca2eb5358fb0a1dfb87
Sri Hari <pokkimgarisrihari5@gmail.com>
2025-09-04 19:44:39 +0530

    add some funtionalities in post.controllers

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -2,6 +2,7 @@ import Post from "../dataBase/models/post.model.js";
 import sharp from "sharp";
 import cloudinary from "../utils/cloudinary.js";
 import User from "../dataBase/models/user.model.js";
+import Comment from "../dataBase/models/comment.model.js";
 
 const handleResponse = (res, status, massage, data = null) => {
   res.status(status).json({
@@ -91,5 +92,77 @@ export const getAutherPost = async (req, res) => {
   } catch (error) {
     console.log(error);
     handleResponse(res, 500, "something went wrong", error);
+  }
+};
+
+export const likePost = async (req, res) => {
+  try {
+    const autherId = req.id;
+    const { postId } = req.params;
+    const post = await Post.findById(postId);
+    if (!post) return handleResponse(res, 404, "post not found");
+    await post.updateOne({ $addToSet: { likes: autherId } });
+    await post.save();
+
+    //implement socket.io for real time notification.
+    return handleResponse(res, 200, "post liked");
+  } catch (error) {
+    console.log(error);
+    return handleResponse(res, 500, "something went wrong", error);
+  }
+};
+
+export const disLikePost = async (req, res) => {
+  try {
+    const autherId = req.id;
+    const { postId } = req.params;
+    const post = await Post.findById(postId);
+    if (!post) return handleResponse(res, 404, "post not found");
+    await post.updateOne({ $pull: { likes: autherId } });
+    await post.save();
+    return handleResponse(res, 200, "post disliked");
+  } catch (error) {
+    console.log(error);
+    return handleResponse(res, 500, "something went wrong", error);
+  }
+};
+
+export const setComment = async (req, res) => {
+  try {
+    const autherId = req.id;
+    const { postId } = req.params;
+    const { text } = req.body;
+    if (!text) return handleResponse(res, 400, "text required");
+    const comment = await Comment.create({
+      text,
+      auther: autherId,
+      post: postId,
+    }).populate({
+      path: "auther",
+      select: "username profilePicture",
+    });
+    const post = await Post.findById(postId);
+    if (post) {
+      post.comments.push(comment._id);
+      post.save();
+    }
+    return handleResponse(res, 200, "post commented");
+  } catch (error) {
+    console.log(error);
+    return handleResponse(res, 500, "somethimg went wrong", error);
+  }
+};
+
+export const getCommentsOfPost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const comments = await Comment.find({ post: postId })
+      .sort({ createdAt: -1 })
+      .populate("auther", "username profilePicture");
+    if (!comments) return handleResponse(res, 404, "comments not found");
+    return handleResponse(res, 200, "fetched all comments", comments);
+  } catch (error) {
+    console.log(error);
+    return handleResponse(res, 500, "something went wrong", error);
   }
 };
