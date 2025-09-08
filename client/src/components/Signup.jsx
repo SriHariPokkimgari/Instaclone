eaedba2931f82949d2a1d351787a46e057d47156
Sri Hari <pokkimgarisrihari5@gmail.com>
2025-09-08 22:02:47 +0530

    modified signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Label } from "@radix-ui/react-label";
+import React, { useState } from "react";
+import { Input } from "./ui/input";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-gray-100">
+      <form>
+        <div>
+          <h2>logo</h2>
+          <p>signup for login... </p>
+        </div>
+        <div>
+          <Label>username</Label>
+          <Input
+            placeholder="username"
+            value={formData.username}
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+          />
+          <Label>email</Label>
+          <Input
+            type="email"
+            placeholder="email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          <Label>password</Label>
+          <Input
+            type="password"
+            placeholder="password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
