60f24f73aec0ffb637e999085882244538046f6b
Sri Hari <pokkimgarisrihari5@gmail.com>
2025-09-09 21:43:00 +0530

    create a signup function

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,6 +1,8 @@
 import { Label } from "@radix-ui/react-label";
 import React, { useState } from "react";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import axios from "../axios";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -8,41 +10,66 @@ const Signup = () => {
     email: "",
     password: "",
   });
+  const [message, setMessage] = useState(null);
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("user/signup", formData);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-gray-100">
-      <form>
-        <div>
-          <h2>logo</h2>
-          <p>signup for login... </p>
+    <div className="flex items-center w-screen h-screen justify-center">
+      <form
+        onSubmit={handleSignup}
+        className="shadow-lg rounded-lg flex flex-col gap-5 p-8"
+      >
+        <div className="my-4">
+          <h2 className="text-center font-bold text-xl ">logo</h2>
+          <p className="text-sm text-center ">
+            Create your account and start your adventure
+          </p>
         </div>
         <div>
-          <Label>username</Label>
+          <Label className="font-medium">Username</Label>
           <Input
-            placeholder="username"
             value={formData.username}
+            className="focus-visible:ring-transparent my-2"
             onChange={(e) =>
               setFormData({ ...formData, username: e.target.value })
             }
+            required
           />
-          <Label>email</Label>
+        </div>
+        <div>
+          <Label className="font-medium">Email</Label>
           <Input
             type="email"
-            placeholder="email"
             value={formData.email}
+            className="focus-visible:ring-transparent my-2"
             onChange={(e) =>
               setFormData({ ...formData, email: e.target.value })
             }
+            required
           />
-          <Label>password</Label>
+        </div>
+        <div>
+          <Label className="font-medium">Password</Label>
           <Input
             type="password"
-            placeholder="password"
             value={formData.password}
+            className="focus-visible:ring-transparent my-2"
             onChange={(e) =>
               setFormData({ ...formData, password: e.target.value })
             }
+            required
           />
         </div>
+        <Button>Singup</Button>
       </form>
     </div>
   );
